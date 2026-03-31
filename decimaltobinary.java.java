bc574449ad24d36a4858967be7f3bc396d306afe
Dua008 <aneesdua779@gmail.com>
2026-03-31 19:33:09 +0500

    Add files via upload

---
+++ decimaltobinary.java.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class decimaltobinary {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int a, b, c;
+
+        System.out.print("Enter an integer: ");
+        
+        a = input.nextInt();   
+        c = a;                 
+
+        String binary = "";
+
+        while (a > 0) {
+            if (a % 2 == 0) {
+                a = a / 2;
+                b = 0;
+            } else {
+                a = a - 1;
+                a = a / 2;
+                b = 1;
+            }
+
+            binary = b + binary;
+        }
+
+        System.out.println("The binary conversion of " + c + " is " + binary);
+    }
+}
